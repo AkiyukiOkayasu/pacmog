52decad8ec14af18a7c5b7b45f95bd60ff3aa179
Akiyuki Okayasu <akiyuki@tkrworks.net>
2022-11-02 18:50:27 +0900

    Remove unnecessary print

--- src/lib.rs
+++ src/lib.rs
@@ -83,7 +83,6 @@ impl<'a> PcmReader<'a> {
 
         //WAVの場合
         if let Ok((input, riff)) = wav::parse_riff_header(input) {
-            println!("Riff length in bytes: {}", riff.size);
             assert_eq!(riff.id, wav::RiffIdentifier::Wave);
             assert_eq!((file_length - 8) as u32, riff.size);
             if let Ok((_, _)) = reader.parse_wav(input) {
