2bf468ece123455ca9e71e5ae78ae528cccb66c1
Akiyuki Okayasu <akiyuki@tkrworks.net>
2022-11-02 23:07:59 +0900

    Add some benches

--- benches/bench.rs
+++ benches/bench.rs
@@ -5,12 +5,18 @@ use pacmo::PcmReader;
 use test::Bencher;
 
 #[bench]
-fn bench_one(b: &mut Bencher) {
+fn parse_wav(b: &mut Bencher) {
     let wav = include_bytes!("../tests/resources/Sine440Hz_1ch_48000Hz_16.wav");
     b.iter(|| {
-        let reader = PcmReader::read_bytes(wav);
-        for i in 0..100 {
-            let _sample = reader.read_sample(0, i).unwrap();
-        }
+        let _reader = PcmReader::read_bytes(wav);
+    });
+}
+
+#[bench]
+fn read_sample(b: &mut Bencher) {
+    let wav = include_bytes!("../tests/resources/Sine440Hz_1ch_48000Hz_16.wav");
+    let reader = PcmReader::read_bytes(wav);
+    b.iter(|| {
+        let _sample = reader.read_sample(0, 0).unwrap();
     });
 }
