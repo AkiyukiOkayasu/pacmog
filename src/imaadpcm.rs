6c7896761330e3f1f6665d8e44c31edd2cd3f7a3
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2023-01-13 03:22:28 +0900

    Fix IMA-ADPCM larger than 2 ch could not be read

--- src/imaadpcm.rs
+++ src/imaadpcm.rs
@@ -2,11 +2,12 @@
 
 use crate::{AudioFormat, PcmReader, PcmSpecs};
 use anyhow::ensure;
-use core::panic;
-use nom::{
-    number::complete::{le_i16, le_i8, le_u8},
-    IResult,
-};
+use heapless::spsc::Queue;
+use nom::bits::{bits, complete::take};
+use nom::error::Error;
+use nom::number::complete::{le_i16, le_i8, le_u8};
+use nom::sequence::tuple;
+use nom::IResult;
 
 /// Index table for STEP_SIZE_TABLE.
 const INDEX_TABLE: [i8; 16] = [-1, -1, -1, -1, 2, 4, 6, 8, -1, -1, -1, -1, 2, 4, 6, 8];
@@ -19,6 +20,8 @@ const STEP_SIZE_TABLE: [i16; 89] = [
     2499, 2749, 3024, 3327, 3660, 4026, 4428, 4871, 5358, 5894, 6484, 7132, 7845, 8630, 9493,
     10442, 11487, 12635, 13899, 15289, 16818, 18500, 20350, 22385, 24623, 27086, 29794, 32767,
 ];
+
+const MAX_NUM_CHANNELS: usize = 2;
 
 /// IMA-ADPCMの各ブロックのHeaderから読み出す情報.
 /// * 'i_samp_0' - The first sample value of the block. When decoding, this will be used as the previous sample to start decoding with.
@@ -110,14 +113,19 @@ pub(crate) fn calc_num_samples_per_channel(
 
 /// IMA-ADPCMファイルを再生するために高レベルにまとめられたクラス
 /// * 'reader' - PCMファイルの低レベル情報にアクセスするためのクラス
-/// * 'reading_buffer' - 再生中のバッファー。get_next_frame()で使用する。
+/// * 'frame_index' - 現在の読んでいるサンプル位置
+/// * 'last_predicted_sample' - デコードされた直近の値
+/// * 'step_size_table_index' - STEP_SIZE_TABLEのどれを示すかのindex
+/// * 'reading_block' - 現在読み込み中のIMA-ADPCMのブロック
+/// * 'nibble_queue' - word読み込み時のnibbleを保管するqueue
 #[derive(Default)]
 pub struct ImaAdpcmPlayer<'a> {
     pub reader: PcmReader<'a>,
     frame_index: u32,
-    last_predicted_sample: [i16; 2],
-    step_size_table_index: [i8; 2],
+    last_predicted_sample: [i16; MAX_NUM_CHANNELS],
+    step_size_table_index: [i8; MAX_NUM_CHANNELS],
     reading_block: &'a [u8],
+    nibble_queue: [Queue<u8, 9>; MAX_NUM_CHANNELS], //todo Queueサイズは2の冪乗の方がパフォーマンスよい。
 }
 
 impl<'a> ImaAdpcmPlayer<'a> {
@@ -136,7 +144,6 @@ impl<'a> ImaAdpcmPlayer<'a> {
     /// * 'out' - サンプルが書き込まれるバッファー
     pub fn get_next_frame(&mut self, out: &mut [i16]) -> anyhow::Result<()> {
         let num_channels = self.reader.specs.num_channels;
-        let samples_per_block = self.reader.specs.ima_adpcm_num_samples_per_block.unwrap() as u32;
 
         // outバッファーのチャンネル数が不足
         ensure!(
@@ -151,7 +158,7 @@ impl<'a> ImaAdpcmPlayer<'a> {
         );
 
         //IMA-ADPCMのBlock切り替わりかどうか判定
-        if self.frame_index % samples_per_block == 0 {
+        if self.reading_block.len() == 0 && self.nibble_queue[0].is_empty() {
             self.update_block();
             for ch in 0..num_channels as usize {
                 out[ch] = self.last_predicted_sample[ch];
@@ -160,11 +167,25 @@ impl<'a> ImaAdpcmPlayer<'a> {
             return Ok(());
         }
 
-        //チャンネル読み出し
+        // 次のData wordsをチャンネル数分よみこむ.
+        if self.nibble_queue[0].is_empty() {
+            for ch in 0..num_channels as usize {
+                let (remains, nibbles) = parse_data_word(self.reading_block).unwrap();
+                self.reading_block = remains;
+                self.nibble_queue[ch].enqueue(nibbles.1).unwrap();
+                self.nibble_queue[ch].enqueue(nibbles.0).unwrap();
+                self.nibble_queue[ch].enqueue(nibbles.3).unwrap();
+                self.nibble_queue[ch].enqueue(nibbles.2).unwrap();
+                self.nibble_queue[ch].enqueue(nibbles.5).unwrap();
+                self.nibble_queue[ch].enqueue(nibbles.4).unwrap();
+                self.nibble_queue[ch].enqueue(nibbles.7).unwrap();
+                self.nibble_queue[ch].enqueue(nibbles.6).unwrap();
+            }
+        }
+
+        //デコード
         for ch in 0..num_channels as usize {
-            let nibble = self
-                .get_nibble(ch as u16, self.frame_index % samples_per_block)
-                .unwrap();
+            let nibble = self.nibble_queue[ch].dequeue().unwrap();
             let (predicted_sample, table_index) = decode_sample(
                 nibble,
                 self.last_predicted_sample[ch],
@@ -185,76 +206,41 @@ impl<'a> ImaAdpcmPlayer<'a> {
         let block_align = self.reader.specs.ima_adpcm_num_block_align.unwrap() as u32;
         let offset = (self.frame_index / samples_per_block) * block_align;
         self.reading_block = &self.reader.data[offset as usize..(offset + block_align) as usize]; //新しいBlockをreading_blockへ更新
+
         assert_eq!(self.reading_block.len(), block_align as usize);
 
         for ch in 0..self.reader.specs.num_channels as usize {
             // BlockのHeader wordを読み出す
-            let (_, block_header) = parse_block_header(&self.reading_block[ch * 4..]).unwrap(); //Headerの1ch分は4byte
+            let (block, block_header) = parse_block_header(&self.reading_block).unwrap(); //Headerの1ch分は4byte
             self.last_predicted_sample[ch] = block_header.i_samp_0;
             self.step_size_table_index[ch] = block_header.b_step_table_index;
+            self.reading_block = block;
         }
-    }
-
-    /// Get nibble from current block.
-    /// * 'channel' - [0, 1]
-    /// * 'frame_index_in_block' - [0..num_samples_per_block]
-    fn get_nibble(&self, channel: u16, frame_index_in_block: u32) -> Option<u8> {
-        let num_samples_per_block =
-            self.reader.specs.ima_adpcm_num_samples_per_block.unwrap() as u32;
-        let num_channels = self.reader.specs.num_channels;
-        let header_offset = 4 * num_channels; //Headerの1ch分は4byte
-        if frame_index_in_block >= num_samples_per_block {
-            panic!();
-        }
-
-        if frame_index_in_block == 0 {
-            //0サンプル目はヘッダーに16bitで記録されているのでnibbleは無い
-            return None;
-        }
-
-        let frame_index_in_block = frame_index_in_block - 1; //Block最初のサンプルはHeaderに16bitで記録されているので1を引く
-        let index = (num_channels as u32 * frame_index_in_block) / 2;
-        let lower4bit = (num_channels as u32 * frame_index_in_block) % 2 == 0;
-        let byte = self.reading_block[header_offset as usize + index as usize];
-        let nibble = u8_to_nibble(byte, lower4bit);
-        Some(nibble)
     }
 }
 
-fn u8_to_nibble(v: u8, lower4bit: bool) -> u8 {
-    let v = if lower4bit {
-        //下位4bit
-        v & 0b00001111u8
-    } else {
-        //上位4bit
-        (v >> 4) & 0b00001111u8
-    };
-    v
+/// IMA-ADPCMのBlockのData word（32bit長）を8つのnibble(4bit長)にパースする.
+fn parse_data_word(input: &[u8]) -> IResult<&[u8], (u8, u8, u8, u8, u8, u8, u8, u8)> {
+    bits::<_, _, Error<(&[u8], usize)>, _, _>(tuple((
+        take(4usize),
+        take(4usize),
+        take(4usize),
+        take(4usize),
+        take(4usize),
+        take(4usize),
+        take(4usize),
+        take(4usize),
+    )))(input)
 }
 
 #[cfg(test)]
 mod tests {
     use crate::imaadpcm::decode_sample;
 
-    use super::u8_to_nibble;
-
     #[test]
     fn ima_adpcm_decode() {
         let (sample, step_size_table_index) = decode_sample(3, -30976, 24);
         assert_eq!(sample, -30913); //0x873F
         assert_eq!(step_size_table_index, 23);
-    }
-
-    #[test]
-    fn nibble() {
-        let t = 1u8;
-        let o = u8_to_nibble(t, true);
-        assert_eq!(o, 1u8);
-        let t = 4u8;
-        let o = u8_to_nibble(t, true);
-        assert_eq!(o, 4);
-        let t = 7u8;
-        let o = u8_to_nibble(t, true);
-        assert_eq!(o, 7);
     }
 }
