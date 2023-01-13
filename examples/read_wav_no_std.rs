1c74a09e7058072b3860e2d3314376853e389e20
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2023-01-13 15:41:11 +0900

    Add example of working with no_std environment

---
+++ examples/read_wav_no_std.rs
@@ -0,0 +1,13 @@
+//! Read wav file in no_std environment.
+
+#![no_std]
+
+use pacmog::PcmReader;
+
+fn main() {
+    let wav = include_bytes!("../tests/resources/Sine440Hz_1ch_48000Hz_16.wav");
+    let reader = PcmReader::new(wav);
+    for sample in 0..48000 {
+        let _s = reader.read_sample(0, sample);
+    }
+}
