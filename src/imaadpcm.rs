868b6585ec7675ee826636ee13a6db077029a702
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2023-01-31 09:37:25 +0900

    Add example in IMA-ADPCM module

--- src/imaadpcm.rs
+++ src/imaadpcm.rs
@@ -1,4 +1,20 @@
 //! IMA-ADPCM
+//!
+//! # Examples
+//!
+//! Play a IMA-ADPCM file.
+//! ```
+//! use pacmog::imaadpcm::{ImaAdpcmPlayer, I1F15};
+//!
+//! let data = include_bytes!("../tests/resources/Sine440Hz_1ch_48000Hz_4bit_IMAADPCM.wav");
+//! let mut player = ImaAdpcmPlayer::new(data);
+//! let mut buffer: [I1F15; 2] = [I1F15::ZERO, I1F15::ZERO];
+//! let b = buffer.as_mut_slice();
+//!
+//! for _ in 0..48000 {
+//!     player.get_next_frame(b).unwrap();
+//! }
+//! ```
 
 use crate::{AudioFormat, PcmReader, PcmSpecs};
 use anyhow::ensure;
