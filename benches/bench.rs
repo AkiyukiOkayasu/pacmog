178746282b7ef1068c0742c8d3759f73f5d1109b
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2025-05-16 16:53:04 +0900

    clippy

--- benches/bench.rs
+++ benches/bench.rs
@@ -14,13 +14,13 @@ static IMAADPCM_SINE_STEREO_48: &[u8] =
 
 #[divan::bench(args = [&WAV_SINE_MONO_48_16_BIT, &WAV_SINE_MONO_48_32_FP, &TANK, &MLK_DREAM, &IMAADPCM_SINE_MONO_48, &IMAADPCM_SINE_STEREO_48])]
 fn parse_wav(input: &[u8]) {
-    let mut input = &input[..];
+    let mut input = input;
     let _reader = PcmReader::new(&mut input).unwrap();
 }
 
 #[divan::bench(args = [&WAV_SINE_MONO_48_16_BIT, &WAV_SINE_MONO_48_32_FP, &TANK, &MLK_DREAM])]
 fn read_sample(input: &[u8]) {
-    let mut input = &input[..];
+    let mut input = input;
     let reader = PcmReader::new(&mut input).unwrap();
     let pcm_specs = reader.get_pcm_specs();
 
@@ -36,7 +36,7 @@ fn read_sample(input: &[u8]) {
     args = [&WAV_SINE_MONO_48_16_BIT, &WAV_SINE_MONO_48_32_FP, &TANK, &MLK_DREAM]
 )]
 fn linear_pcm_player<T: num_traits::float::Float>(input: &[u8]) {
-    let mut input = &input[..];
+    let mut input = input;
     let reader = PcmReader::new(&mut input).unwrap();
     let mut player = PcmPlayer::new(reader);
     let mut buffer: [T; 2] = [T::zero(), T::zero()];
@@ -50,7 +50,7 @@ fn linear_pcm_player<T: num_traits::float::Float>(input: &[u8]) {
 
 #[divan::bench(args = [&IMAADPCM_SINE_MONO_48, &IMAADPCM_SINE_STEREO_48])]
 fn ima_adpcm_player(input: &[u8]) {
-    let mut input = &input[..];
+    let mut input = input;
     let mut buffer: [I1F15; 2] = [I1F15::ZERO, I1F15::ZERO];
     let mut player = ImaAdpcmPlayer::new(&mut input).unwrap();
     let buf = buffer.as_mut_slice();
